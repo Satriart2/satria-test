979a0a18b2e9dfa5a2bb49530103037ae45df195
Satriart2 <mobilberjalan20@gmail.com>
2024-11-01 22:09:17 +0800

    tambah up file

---
+++ main.js
@@ -0,0 +1,18 @@
+const btn1 = document.getElementById('btn1')
+const btn2 = document.querySelector('.btn2')
+
+btn2.style.background = 'red'
+
+const 
+
+function baru(){
+    btn2.style.background = 'aqua'
+}
+
+function ganti(){
+    
+}
+
+function deff(){
+    console.log('aman')
+}
